d9f134441444335036bbfdb109ee294321364589
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-10-06 15:02:57 +0300

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,14 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('Learning Angular'),
-    // new WishItem('Get coffe!', true),
-    // new WishItem('Achive your goals!')
+    new WishItem('Learning Angular'),
+    new WishItem('Get coffe!', true),
+    new WishItem('Achive your goals!')
   ]
   title = 'wishlist';
+
+  toggleItem(item : WishItem){
+    item.isCompleted = !item.isCompleted
+    console.log(item)
+  }
 }
