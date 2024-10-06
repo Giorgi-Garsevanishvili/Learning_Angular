91ea7b60a49104a1570bd4f9cd8f2139ef971fa5
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-10-06 14:34:21 +0300

    Starting to Learn Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/modules/wishItems';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  items : WishItem[] = [
+    // new WishItem('Learning Angular'),
+    // new WishItem('Get coffe!', true),
+    // new WishItem('Achive your goals!')
+  ]
+  title = 'wishlist';
+}
